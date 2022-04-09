ff4b0dd1127bee1a916c35e1cae310547d05eddf
codedog06 <codedog0606@gmail.com>
2022-04-09 22:53:12 +0800

    update patchTodo.js

---
+++ patchTodo.js
@@ -0,0 +1,33 @@
+function patchTodo() {
+ const headers = {
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'PATCH, POST, GET, OPTIONS, DELETE',
+  'Content-Type': 'application/json'
+ }
+
+ request.on('end', () => {
+  try {
+   const todo = JSON.parse(body).title;
+   const id = request.url.split('/').pop();
+   const index = todos.findIndex(el => el.id == id);
+   // 檢查title是否有值, todo ID是否存在 -1代表不存在
+   if (todo !== undefined && id !== -1) {
+    todos[index].title = todo;
+    res.writeHead(200, headers);
+    res.write(JSON.stringify({
+     'status': 'scuess',
+     'data': todos,
+    }));
+    res.end();
+   } else {
+    errHandle(res);
+   }
+   res.end();
+  } catch {
+   errHandle(res);
+  }
+ });
+}
+
+module.exports = patchTodo;
