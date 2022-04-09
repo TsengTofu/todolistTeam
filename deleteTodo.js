7376d05c0328afbd392686a66671b51a35124f0d
Joseph <yagerkiller@gmai.com>
2022-04-09 22:09:09 +0800

    Update: deleteTodo

---
+++ deleteTodo.js
@@ -0,0 +1,28 @@
+function deleteTodo(res) {
+  const id = req.url.split("/").pop();
+  const index = todos.findIndex((element) => element.id === id);
+
+  if (index !== -1) {
+    todos.splice(index, 1);
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: "success",
+        data: todos,
+      })
+    );
+    res.end();
+  } else if (index === -1) {
+    todos.length = 0;
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: "success",
+        data: todos,
+      })
+    );
+    res.end();
+  } else {
+    errHandle(res);
+  }
+}
