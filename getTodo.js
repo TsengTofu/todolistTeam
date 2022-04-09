223dfdfe4ab47a8e8b0ebaf98f33c533d0f352dc
owen <>
2022-04-09 23:14:31 +0800

    feat: 串接 getTodo API 到 server.js

--- getTodo.js
+++ getTodo.js
@@ -1,4 +1,10 @@
 const getTodo = (res, todos) => {
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    }
     res.writeHead(200, headers)
     res.write(JSON.stringify(
         {
