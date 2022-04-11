318e78e9c32383cf718b9b54e0d62cb0f6fb43c8
codedog06 <codedog0606@gmail.com>
2022-04-11 10:11:10 +0800

    modify patchTodo.js - update PR feedback

--- patchTodo.js
+++ patchTodo.js
@@ -1,15 +1,16 @@
-function patchTodo() {
- const headers = {
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'PATCH, POST, GET, OPTIONS, DELETE',
-  'Content-Type': 'application/json'
- }
+const errHandle = require('./errorHandle');
+const headers = {
+ 'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+ 'Access-Control-Allow-Origin': '*',
+ 'Access-Control-Allow-Methods': 'PATCH, POST, GET, OPTIONS, DELETE',
+ 'Content-Type': 'application/json'
+}
 
- request.on('end', () => {
+function patchTodo(res, req, todos) {
+ req.on('end', () => {
   try {
    const todo = JSON.parse(body).title;
-   const id = request.url.split('/').pop();
+   const id = req.url.split('/').pop();
    const index = todos.findIndex(el => el.id == id);
    // 檢查title是否有值, todo ID是否存在 -1代表不存在
    if (todo !== undefined && id !== -1) {
@@ -29,5 +30,4 @@ function patchTodo() {
   }
  });
 }
-
 module.exports = patchTodo;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 const errHandle = require('./errorHandle');
+const getTodo = require('./getTodo');
+const postTodo = require('./postTodo');
+const deleteTodo = require('./deleteTodo');
+const patchTodo = require('./patchTodo');
 const todos = [];
 
-const requestListener = (req, res)=>{
+const requestListener = (req, res) => {
     const headers = {
         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
         'Access-Control-Allow-Origin': '*',
@@ -11,26 +15,56 @@ const requestListener = (req, res)=>{
         'Content-Type': 'application/json'
     }
     let body = "";
-    
-    req.on('data', chunk=>{
-        body+=chunk;
+
+    req.on('data', chunk => {
+        body += chunk;
     })
-    
-    if(req.url=="/todos" && req.method == "GET"){
-        // getTodo.js
-    }else if(req.url=="/todos" && req.method == "POST"){
-        // postTodo.js
-    }else if(req.url=="/todos" && req.method == "DELETE"){
-        // deleteTodo.js
-    }else if(req.url.startsWith("/todos/") && req.method=="DELETE"){
-        // deleteTodo.js
-    }else if(req.url.startsWith("/todos/") && req.method=="PATCH"){
-        // patchTodo.js
-    }else if(req.method == "OPTIONS"){
-        res.writeHead(200,headers);
+
+    if (req.url == "/todos" && req.method == "GET") {
+        res.writeHead(200, headers);
+        res.write(JSON.stringify({
+            'status': 'scuess',
+            'data': todos,
+        }));
         res.end();
-    }else{
-        res.writeHead(404,headers);
+        // getTodo.js
+    } else if (req.url == "/todos" && req.method == "POST") {
+        req.on('end', () => { // 確保body有資料
+            // try catch 檢查是否為物件格式, 但有可能是物件格式但key不是title 如: {'qq': title}
+            try {
+             const title = JSON.parse(body).title;
+             if (title !== undefined) {
+              const todo = {
+               "title": title,
+               "id": uuid4()
+              };
+              todos.push(todo);
+              res.writeHead(200, headers);
+              res.write(JSON.stringify({
+               'status': 'scuess',
+               'data': todos,
+              }));
+              res.end();
+             } else {
+              errHandle(res);
+             }
+            } catch (error) {
+             errHandle(res);
+            }
+           });
+        // postTodo.js
+    } else if (req.url == "/todos" && req.method == "DELETE") {
+        // deleteTodo.js
+    } else if (req.url.startsWith("/todos/") && req.method == "DELETE") {
+        // deleteTodo.js
+    } else if (req.url.startsWith("/todos/") && req.method == "PATCH") {
+        // patchTodo.js
+        patchTodo(res, req, todos);
+    } else if (req.method == "OPTIONS") {
+        res.writeHead(200, headers);
+        res.end();
+    } else {
+        res.writeHead(404, headers);
         res.write(JSON.stringify({
             "status": "false",
             "message": "無此網站路由"
