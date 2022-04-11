857e91041260661af0783682846fc6f67f8fd57a
codedog06 <codedog0606@gmail.com>
2022-04-11 15:53:54 +0800

    Modify patchTodo.js, server.js - change patchTodo.js call function method and add call function on server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 const errHandle = require('./errorHandle');
-const getTodo = require('./getTodo');
-const postTodo = require('./postTodo');
-const deleteTodo = require('./deleteTodo');
 const patchTodo = require('./patchTodo');
 const todos = [];
 
@@ -21,37 +18,8 @@ const requestListener = (req, res) => {
     })
 
     if (req.url == "/todos" && req.method == "GET") {
-        res.writeHead(200, headers);
-        res.write(JSON.stringify({
-            'status': 'scuess',
-            'data': todos,
-        }));
-        res.end();
         // getTodo.js
     } else if (req.url == "/todos" && req.method == "POST") {
-        req.on('end', () => { // 確保body有資料
-            // try catch 檢查是否為物件格式, 但有可能是物件格式但key不是title 如: {'qq': title}
-            try {
-             const title = JSON.parse(body).title;
-             if (title !== undefined) {
-              const todo = {
-               "title": title,
-               "id": uuid4()
-              };
-              todos.push(todo);
-              res.writeHead(200, headers);
-              res.write(JSON.stringify({
-               'status': 'scuess',
-               'data': todos,
-              }));
-              res.end();
-             } else {
-              errHandle(res);
-             }
-            } catch (error) {
-             errHandle(res);
-            }
-           });
         // postTodo.js
     } else if (req.url == "/todos" && req.method == "DELETE") {
         // deleteTodo.js
